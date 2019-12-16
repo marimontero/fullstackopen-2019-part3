5a8477bbbbf18cc77f222cdacadc4a3fcb62cf19
marimontero <maria.montero@devex.com>
2019-12-16 13:09:09 +0100

    Part 3 Exc.3.11 Phonebook Full stack

---
+++ build/precache-manifest.ed9473b3c4e08af169e12cd8d7346211.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "668c61f7d89f3d53c4222ded08fdc549",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06956ca29cf79db0f0f0",
+    "url": "/static/css/main.f09409a1.chunk.css"
+  },
+  {
+    "revision": "e4031a46f514e94a1588",
+    "url": "/static/js/2.fe319e35.chunk.js"
+  },
+  {
+    "revision": "06956ca29cf79db0f0f0",
+    "url": "/static/js/main.7b2b70fb.chunk.js"
+  },
+  {
+    "revision": "4b475c9cb72c4e6c8fdb",
+    "url": "/static/js/runtime-main.3453b3e7.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(bodyParser.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('content-body', (request, response) => {
   return JSON.stringify(request.body)
